4baad7549e14dc307f423d23eef1bb9a93350fac
RavengerINC <jamesmdalziel@gmail.com>
2026-09-04 23:58:59 -0300

    Init

---
+++ Assets/Code/Tests/SmokeTests.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Code.Tests
+{
+    public class SmokeTests
+    {
+        [Test]
+        public void OnePlusOne_EqualsTwo()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
